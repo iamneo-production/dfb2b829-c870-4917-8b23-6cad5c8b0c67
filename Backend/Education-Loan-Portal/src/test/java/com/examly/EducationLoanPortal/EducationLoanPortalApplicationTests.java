eebceebf469a5a79526e2b78bc81c161cbeffef9
udayagiricharan <udayagiricharankumar@gmail.com>
2023-05-03 23:32:05 +0530

    updated the table user

---
+++ Backend/Education-Loan-Portal/src/test/java/com/examly/EducationLoanPortal/EducationLoanPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examly.EducationLoanPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationLoanPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
