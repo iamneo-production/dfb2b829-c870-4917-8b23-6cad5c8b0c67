47d71f9ca8b0dc8d2bd5b5c87e036d319baf48f5
udayagiricharan <udayagiricharankumar@gmail.com>
2023-05-05 23:07:54 +0530

    updated the user function

--- Backend/Education-Loan-Portal/Education-Loan-Portal/src/main/java/EducationLoanPortal/Education/Loan/Portal/model/User.java
+++ Backend/Education-Loan-Portal/Education-Loan-Portal/src/main/java/EducationLoanPortal/Education/Loan/Portal/model/User.java
@@ -1,2 +1,83 @@
-package EducationLoanPortal.Education.Loan.Portal.model;public class User {
+package EducationLoanPortal.Education.Loan.Portal.model;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+ private String firstName;
+ private String lastName;
+ private String email;
+ private String password;
+ private String address;
+ private String phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+    return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+            + ", password=" + password + ", address=" + address + ", phoneNumber=" + phoneNumber + "]";
+    }
+    
 }
