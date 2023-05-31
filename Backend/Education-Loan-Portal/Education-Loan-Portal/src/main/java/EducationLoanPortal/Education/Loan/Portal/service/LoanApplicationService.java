53cc61641ae2afb28cd25f1b5eb9bc016918623e
Raghul S <90319053+RaghulBrynn@users.noreply.github.com>
2023-05-31 11:48:29 +0530

    Updated the edit option

--- Backend/Education-Loan-Portal/Education-Loan-Portal/src/main/java/EducationLoanPortal/Education/Loan/Portal/service/LoanApplicationService.java
+++ Backend/Education-Loan-Portal/Education-Loan-Portal/src/main/java/EducationLoanPortal/Education/Loan/Portal/service/LoanApplicationService.java
@@ -1,10 +1,12 @@
 package EducationLoanPortal.Education.Loan.Portal.service;
 
+import EducationLoanPortal.Education.Loan.Portal.model.Loan;
 import EducationLoanPortal.Education.Loan.Portal.model.LoanApplication;
 import EducationLoanPortal.Education.Loan.Portal.repository.LoanApplicationRepo;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
@@ -24,6 +26,29 @@ public class LoanApplicationService {
         // loanApplication.setUser_id(loanApplication.getUser().getId());
 
         return loanApplicationRepo.save(loanApplication);
+    }
+
+    // get loan application by id with exception handling
+    public LoanApplication getLoanApplicationById(Long id) {
+        return loanApplicationRepo.findLoanApplicationById(id)
+                .orElseThrow(() -> new RuntimeException("Loan application by id " + id + " was not found"));
+    }
+
+    public LoanApplication updateLoanApplicationById(Long id, LoanApplication loanApplication) {
+        Optional<LoanApplication> existingLoanApplication = loanApplicationRepo.findById(id);
+        if (!existingLoanApplication.isPresent()) {
+            throw new RuntimeException("Loan application not found with id: " + id);
+        }
+        // get the loan application by id
+        var loanApplication_var = existingLoanApplication.get();
+        // set the loan application purpose, loan amount, status and application date
+        loanApplication_var.setPurpose(loanApplication.getPurpose());
+        loanApplication_var.setLoanAmount(loanApplication.getLoanAmount());
+        loanApplication_var.setStatus(loanApplication.getStatus());
+        loanApplication_var.setApplicationDate(loanApplication.getApplicationDate());
+        // save the loan application
+        return loanApplicationRepo.save(loanApplication_var);
+
     }
 
     public Optional<LoanApplication> findAllLoanApplications() {
@@ -49,24 +74,16 @@ public class LoanApplicationService {
         }
     }
 
-    public Optional<LoanApplication> findAllByUserId(Long id) {
+    public Optional<LoanApplication> findAllByStatus(String status) {
         try {
-            return loanApplicationRepo.findAllByUserId(id);
+            return loanApplicationRepo.findAllByStatus(status);
         } catch (Exception e) {
-            System.out.println(e);
-            throw new RuntimeException("Error while getting all loan applications by user id");
-        }}
-
-         public Optional<LoanApplication> findAllByStatus(String status){
-         try {
-         return loanApplicationRepo.findAllByStatus(status);
-         } catch (Exception e) {
-         throw new RuntimeException("Error while getting all loan applications");
-         }
-
-        // public void deleteLoanApplication(Long id) {
-        // loanApplicationRepo.deleteLoanApplicationById(id);
-        // }
+            throw new RuntimeException("Error while getting all loan applications");
+        }
 
     }
+
+//    public Optional<LoanApplication> findAllByUserId(Long user) {
+//
+//    }
 }
