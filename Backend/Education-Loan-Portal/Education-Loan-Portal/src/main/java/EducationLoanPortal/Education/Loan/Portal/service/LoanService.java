bc40c05f890f6a7f091b6f5fbaa179eeefcdb7b7
RaghulBrynn <raghulraghu007@gmail.com>
2023-06-05 11:55:43 +0530

    updated

--- Backend/Education-Loan-Portal/Education-Loan-Portal/src/main/java/EducationLoanPortal/Education/Loan/Portal/service/LoanService.java
+++ Backend/Education-Loan-Portal/Education-Loan-Portal/src/main/java/EducationLoanPortal/Education/Loan/Portal/service/LoanService.java
@@ -18,68 +18,68 @@ import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 @Service
 public class LoanService {
 
-    private final LoanRepo loanRepo;
+  private final LoanRepo loanRepo;
 
-    public LoanService(LoanRepo loanRepo) {
-        this.loanRepo = loanRepo;
+  public LoanService(LoanRepo loanRepo) {
+    this.loanRepo = loanRepo;
+  }
+
+  public Loan addLoan(Loan loan) {
+    return loanRepo.save(loan);
+  }
+
+  // get loan by id
+  public Loan getLoanById(Long id) throws UserNotFoundException {
+    return loanRepo.getLoanById(id)
+      .orElseThrow(() -> new UserNotFoundException("Loan by id " + id + " was not found"));
+  }
+
+  // update an existing loan by id
+
+  public Optional<Loan> updateLoanById(Long id, Loan loan) throws ResourceNotFoundException {
+    Optional<Loan> existingLoan = loanRepo.findById(id);
+    if (!existingLoan.isPresent()) {
+      throw new ResourceNotFoundException("Loan not found with id: " + id);
     }
 
-    public Loan addLoan(Loan loan) {
-        return loanRepo.save(loan);
+    return Optional.ofNullable(loanRepo.save(loan));
+  }
+
+  // delete an existing loan by id
+  public void deleteLoan(Long id) throws UserNotFoundException {
+    try {
+      loanRepo.deleteLoanById(id);
+    } catch (Exception e) {
+      throw new UserNotFoundException("Loan by id " + id + " was not found");
     }
+  }
 
-    // get loan by id
-    public Loan getLoanById(Long id) throws UserNotFoundException {
-        return loanRepo.getLoanById(id)
-                .orElseThrow(() -> new UserNotFoundException("Loan by id " + id + " was not found"));
+//  //     get all loans by user id
+//  public List<Loan> findAllLoansByUserId(Long userId) {
+//    // get all loans ids by user id
+//    List<Long> loanIds = loanRepo.findAllLoanIdsByUserId(userId);
+//    // iterate over the list of loan ids and get the loan by id
+//    List<Loan> loans = loanIds.stream().map(loanId -> loanRepo.getLoanById(loanId).get())
+//      .collect(Collectors.toList());
+//
+//    if (loans.isEmpty()) {
+//      // return an empty list if no loans were found
+//      return Collections.emptyList();
+//    } else {
+//      // return the list of loans if at least one loan was found
+//      return loans;
+//    }
+//  }
+
+
+  // get all loans by status
+  public Optional<Loan> findAllLoansByStatus(String status) throws UserNotFoundException {
+    try {
+      return loanRepo.findAllByStatus(status);
+    } catch (Exception e) {
+      throw new UserNotFoundException("Loan by id " + id + " was not found");
     }
-
-    // update an existing loan by id
-
-    public Optional<Loan> updateLoanById(Long id, Loan loan) throws ResourceNotFoundException {
-        Optional<Loan> existingLoan = loanRepo.findById(id);
-        if (!existingLoan.isPresent()) {
-            throw new ResourceNotFoundException("Loan not found with id: " + id);
-        }
-
-        return Optional.ofNullable(loanRepo.save(loan));
-    }
-
-    // delete an existing loan by id
-    public void deleteLoan(Long id) throws UserNotFoundException {
-        try {
-            loanRepo.deleteLoanById(id);
-        } catch (Exception e) {
-            throw new UserNotFoundException("Loan by id " + id + " was not found");
-        }
-    }
-
-//     get all loans by user id
-    public List<Loan> findAllLoansByUserId(Long userId) {
-        // get all loans ids by user id
-        List<Long> loanIds = loanRepo.findAllLoanIdsByUserId(userId);
-        // iterate over the list of loan ids and get the loan by id
-        List<Loan> loans = loanIds.stream().map(loanId -> loanRepo.getLoanById(loanId).get())
-                .collect(Collectors.toList());
-
-        if (loans.isEmpty()) {
-            // return an empty list if no loans were found
-            return Collections.emptyList();
-        } else {
-            // return the list of loans if at least one loan was found
-            return loans;
-        }
-    }
+  }
 
 
-
-    // get all loans by status
-    public Optional<Loan> findAllLoansByStatus(String status) throws UserNotFoundException {
-        try {
-            return loanRepo.findAllByStatus(status);
-        } catch (Exception e) {
-            throw new UserNotFoundException("Loan by id " + id + " was not found");
-        }
-    }
-
-
+}
