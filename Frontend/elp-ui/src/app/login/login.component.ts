d6a01ccc5311eccf9cccb1fe7867bf4761970194
Thasneem17 <113753696+Thasneem17@users.noreply.github.com>
2023-06-11 15:34:33 +0530

    Updated

--- Frontend/elp-ui/src/app/login/login.component.ts
+++ Frontend/elp-ui/src/app/login/login.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+   credentials={
+    username:'',
+    password:''
+   }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSubmit()
+  {
+    console.log("form is submitted");
+    if((this.credentials.username!='' && this.credentials.password!='')&& (this.credentials.username!=null && this.credentials.password!=null))
+    {
+      console.log("We have to submit the form to server")
+    }else{
+      console.log("Fields are empty");
+    }
+  }
 
 }
