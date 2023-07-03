6676f85cbe96e972995b2c55001374637adb5142
ARAVINDVELUSAMY06 <69682312+ARAVINDVELUSAMY06@users.noreply.github.com>
2023-07-03 16:35:38 +0530

    updated sidenavbar.component.spec.ts

---
+++ Frontend/elp-ui/src/app/sidenav bar/sidenavbar.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SidenavComponent } from './sidenav.component';
+
+describe('SidenavComponent', () => {
+  let component: SidenavComponent;
+  let fixture: ComponentFixture<SidenavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SidenavComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SidenavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+Footer
+© 2023 GitHub, Inc.
