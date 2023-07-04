5cafab63618e3a8c5ebd47ae4b10dbee41f6f699
RaghulBrynn <raghulraghu007@gmail.com>
2023-07-04 17:04:59 +0530

    payment updated

--- Frontend/elp-ui/src/app/payment/payment.component.ts
+++ Frontend/elp-ui/src/app/payment/payment.component.ts
@@ -1,7 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserAuthService } from '../_services/user-auth.service';
+import { MatDatepickerControl, MatDatepickerPanel } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
+import { url } from '../config';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-payment',
@@ -9,13 +13,43 @@ import { UserAuthService } from '../_services/user-auth.service';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent {
+  // loan_id:  number=0;
   amount : number=0;
   paymentDate : string='';
+  
+  messageState: string='';
+  
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private dialog: MatDialog){}
   
   submitPaymentForm(form:NgForm){
-    const loanId = UserAuthService.get
+    if (form.valid) {
+      const paymentData = {
+        // loan_id:this.loan_id,
+        amount:this.amount,
+        paymentDate:this.paymentDate
+      };
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      const options = { headers: headers };
+      this.http.post(`${url}/payment`, paymentData, options)
+        .subscribe((res) => {
+          console.log(res);
+          this.openDialog('Success', 'Payment done successfully');
+          form.reset();
+        },
+        (err) => {
+          console.log(err);
+          this.openDialog('Error', 'An error occurred while doing payment');
+        });
+    }
+    
+  }
+  openDialog(title: string, message: string) {
+    this.messageState = title; // Set the messageState based on the title
+    this.dialog.open(DialogComponent, {
+      width: '400px',
+      data: { title, message }
+    });
   }
 }
 
